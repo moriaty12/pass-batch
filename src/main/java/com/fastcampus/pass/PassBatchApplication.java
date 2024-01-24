bfff4670a14b6dcbee6b071c0b5342bf3c6fcae8
DESKTOP-FSBOMA2\User <moriaty12@gmail.com>
2024-01-24 15:02:25 +0900

    스프링 부트 버전 다운 yml 파일 h2 데이터베이스 수정 간단한 job step 실행 확인

--- src/main/java/com/fastcampus/pass/PassBatchApplication.java
+++ src/main/java/com/fastcampus/pass/PassBatchApplication.java
@@ -1,11 +1,46 @@
 package com.fastcampus.pass;
 
+import org.springframework.batch.core.Job;
+import org.springframework.batch.core.Step;
+import org.springframework.batch.core.StepContribution;
+import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing;
+import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
+import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
+import org.springframework.batch.core.scope.context.ChunkContext;
+import org.springframework.batch.core.step.tasklet.Tasklet;
+import org.springframework.batch.repeat.RepeatStatus;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
+@EnableBatchProcessing
 public class PassBatchApplication {
 
+	private final JobBuilderFactory jobBuilderFactory;
+	private  final StepBuilderFactory stepBuilderFactory;
+
+	public PassBatchApplication(JobBuilderFactory jobBuilderFactory, StepBuilderFactory stepBuilderFactory) {
+		this.jobBuilderFactory = jobBuilderFactory;
+		this.stepBuilderFactory = stepBuilderFactory;
+	}
+	@Bean
+	public Step passStep(){
+		return this.stepBuilderFactory.get("passStep")
+				.tasklet((contribution, chunkContext) -> {
+					System.out.println("Excute passStep");
+					return RepeatStatus.FINISHED;
+				}).build();
+	}
+
+	@Bean
+	public Job passJob(){
+		return this.jobBuilderFactory.get("passJob")
+				.start(passStep())
+				.build();
+	}
+	// 스텝을 테스클릿으로하고 그 스텝을 넣었다
 	public static void main(String[] args) {
 		SpringApplication.run(PassBatchApplication.class, args);
 	}
