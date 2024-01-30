2000eb52d3061ffc5ecd06edeb12e5839033005c
DESKTOP-FSBOMA2\User <moriaty12@gmail.com>
2024-01-30 17:37:34 +0900

    테스트 업데이트 삭제 오류 (미완1)

--- src/main/java/com/fastcampus/pass/repository/packaze/PackageRepository.java
+++ src/main/java/com/fastcampus/pass/repository/packaze/PackageRepository.java
@@ -2,11 +2,22 @@ package com.fastcampus.pass.repository.packaze;
 
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
 
+import javax.transaction.Transactional;
 import java.time.LocalDateTime;
 import java.util.List;
 
 
 public interface PackageRepository extends JpaRepository<PackageEntity, Integer> {
     List<PackageEntity> findByCreatedAtAfter(LocalDateTime dateTime, PageRequest packageSeq);
+
+    @Modifying
+    @Transactional
+    @Query(value = "UPDATE PackageEntity p "  +
+                   "   SET p.count = :count," +
+                   "       p.period = :period" +
+                   " WHERE p.packageSeq = :packageSeq")
+    int updateCountAndPeriod(Integer packageSeq, Integer Count, Integer period);
 }
--- src/test/java/com/fastcampus/pass/repository/PackageRepositoryTest.java
+++ src/test/java/com/fastcampus/pass/repository/PackageRepositoryTest.java
@@ -14,15 +14,17 @@ import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Sort;
+import org.springframework.data.querydsl.QPageRequest;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit4.SpringRunner;
 
 import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.util.List;
+import java.util.Optional;
 
 import static org.junit.Assert.*;
-@RunWith(SpringRunner.class)
+@RunWith(SpringRunner.class) // https://n1tjrgns.tistory.com/224 참조
 @Slf4j
 @SpringBootTest
 @ActiveProfiles("test")
@@ -37,13 +39,65 @@ public class PackageRepositoryTest {
         packageEntity.setPackageName("바디 챌린지 PT 12주");
         packageEntity.setPeriod(84);
 
-        System.out.println(packageRepository);
         //when
         packageRepository.save(packageEntity);
 
         //then
         assertNotNull(packageEntity.getPackageSeq());
+    }
 
+    @Test
+    public void test_findByCreatedAtAfter(){
+        // given
+        LocalDateTime dateTime = LocalDateTime.now().minusMinutes(1);
+
+        PackageEntity packageEntity0 = new PackageEntity();
+        packageEntity0.setPackageName("학생 전용 3개월");
+        packageEntity0.setPeriod(90);
+        packageRepository.save(packageEntity0);
+
+        PackageEntity packageEntity1 = new PackageEntity();
+        packageEntity1.setPackageName("학생 전용 6개월");
+        packageEntity1.setPeriod(180);
+        packageRepository.save(packageEntity1);
+
+        // when
+        final List<PackageEntity> packageEntities = packageRepository.findByCreatedAtAfter(dateTime, PageRequest.of(0,1,Sort.by("packageSeq").descending()));
+        // then
+        assertEquals(1,packageEntities.size());
+        /*assertEquals(packageEntity1.getPackageSeq(),packageEntities.get(1));*/
+    }
+
+    @Test
+    public void test_updateCountAndPeriod(){
+        // given
+        PackageEntity packageEntity = new PackageEntity();
+        packageEntity.setPackageName("학생 전용 6개월");
+        packageEntity.setPeriod(180);
+        packageRepository.save(packageEntity);
+
+        // when
+        int updateCount = packageRepository.updateCountAndPeriod(packageEntity.getPackageSeq(),30,120);
+        final PackageEntity updatePackageEntity = packageRepository.findById(packageEntity.getPackageSeq()).get();
+
+        // then
+        assertEquals(1,updateCount);
+        assertEquals(30, Optional.ofNullable(updatePackageEntity.getCount()));
+        assertEquals(120, Optional.ofNullable(updatePackageEntity.getPeriod()));
+    }
+
+    @Test
+    public void test_delet(){
+        //given
+        PackageEntity packageEntity = new PackageEntity();
+        packageEntity.setPackageName("제거할 이용권");
+        packageEntity.setCount(1);
+        PackageEntity newPacakgeEntity = packageRepository.save(packageEntity);
+        //when
+        packageRepository.deleteById(newPacakgeEntity.getPackageSeq());
+
+        //then
+        /*assertEquals(packageRepository.findById(newPacakgeEntity.getPackageSeq()).isEmpty());*/
     }
 
 }
